2b25401cb1c90a7b01d4da46dc90143bd86432fb
shriyash awasthi <shriyashawasthi01feb@gmail.com>
2023-06-15 01:34:47 +0530

    increment decrement counter using button

---
+++ app.js
@@ -0,0 +1,27 @@
+(function(){
+    const buttons=document.querySelectorAll(".counterbutton")
+    let count=0;
+    buttons.forEach(function(button) {
+    button.addEventListener('click',function(){
+        if(button.classList.contains("nextbtn")){
+            count++;
+        }
+        else if (button.classList.contains("prevbtn")){
+            count--;
+        }
+        const counter=document.querySelector('#counter')
+        counter.textContent=count;
+        if(count<0)
+        {
+            counter.style.color='red';
+        }
+        else if(count>0)
+        {
+            counter.style.color='green';
+        }
+        else{
+            counter.style.color='black'
+        }
+    })
+    });
+}) ()
